4d1669c1f97bae3cdd87450aa91fb2376badbb75
ToxicedIsOnTheLand <94137529+ToxicedIsOnTheLand@users.noreply.github.com>
2022-03-28 13:00:44 +0100

    updated main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import disnake
 import discord
+import discord_slash
 import os
+import random
 import time
-import discord.ext
-from discord.utils import get
-from discord.ext import commands, tasks
-from discord.ext.commands import has_permissions,  CheckFailure, check
+import disnake.ext
+from disnake.utils import get
+from disnake.ext import commands, tasks
+from disnake.ext.commands import has_permissions,  CheckFailure, check
 
 # Unreal Bot developers are not allowed to use this
 
@@ -12,7 +15,14 @@ from discord.ext.commands import has_permissions,  CheckFailure, check
 
 # Unreal Bot developers are not allowed to use this
 
-client = discord.Client()
+client = disnake.Client()
+
+intents.members = True
+intents.presences = True
+intents.guild_reactions = True
+intents.reactions = True
+
+client = commands.InteractionBot(test_guilds=[GUILD_ID])
 
 client = commands.Bot(command_prefix = '?') #replace the ? to your own prefix
 
@@ -20,107 +30,107 @@ client = commands.Bot(command_prefix = '?') #replace the ? to your own prefix
 async def on_ready():
   print("your bot is online")
   
-@client.command(name="say")
-@commands.has_permissions(administrator=True)
-async def say(ctx, *, text):
-  await ctx.send(text)
+@client.slash_command()
+async def say(inter, text):
+  await inter.send(text)
 
-@client.command(name="ban")
+@client.slash_command()
 @commands.has_permissions(ban_members = True)
-async def ban(ctx, user : discord.User, *, reason=None):
+async def ban(inter, user : discord.User, *, reason=None):
   try:
     await user.ban(reason=reason)
-    embed = discord.Embed(title="User has been Banned", description=f"{ctx.mention} has been banned by {ctx.author.mention}")
-    await ctx.send(embed=embed)
+    embed = disnake.Embed(title="User has been Banned", description=f"{ctx.mention} has been banned by {ctx.author.mention}")
+    await inter.send(embed=embed)
   except:
-    embed = discord.Embed(title="User cannot be Banned", description=f"cannot ban {ctx.mention}!")
-    await ctx.reply(embed=embed)
+    embed = disnake.Embed(title="User cannot be Banned", description=f"cannot ban {ctx.mention}!")
+    await inter.reply(embed=embed)
 
 @ban.error
-async def clear_error(ctx, error):
+async def clear_error(inter, error):
     if isinstance(error, commands.MissingPermissions):
-        await ctx.send(f"{ctx.message.author.mention} You do not have Permissions to use this Command!")
+        await inter.send(f"{inter.author.mention} You do not have Permissions to use this Command!")
 
 @client.command(name="unban")
 @commands.has_permissions(administrator = True)
-async def unban(ctx, *, member_id: int):
+async def unban(inter, member_id: int):
   try:
-    await ctx.guild.unban(discord.Object(id=member_id))
-    embed = discord.Embed(title="Unbanned User", description="Specific user has been Unbanned")
-    await ctx.reply(embed=embed)
+    await inter.guild.unban(discord.Object(id=member_id))
+    embed = disnake.Embed(title="Unbanned User", description="Specific user has been Unbanned")
+    await inter.send(embed=embed)
   except:
-    embed = discord.Embed(title="Cannot unban User!", description="Cannot unban the specific User!")
-    await ctx.reply(embed=embed)
+    embed = disnake.Embed(title="Cannot unban User!", description="Cannot unban the specific User!")
+    await inter.send(embed=embed)
 
 @unban.error
 async def clear_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
         await ctx.send(f"{ctx.message.author.mention} You do not have Permissions to use this Command!")
  
-@client.command(name="purge", pass_context=True, aliases=['p'])
+@client.command(description="purges messages")
 @commands.cooldown(1, 5, commands.BucketType.user)
 @commands.has_permissions(administrator=True)
-async def clean(ctx, limit: int):
-        message = ctx.message
-        await message.delete()
-
-  
-        await ctx.channel.purge(limit=limit)
-        embedVar = discord.Embed(title="Purge Alert!", description=f"Messages Purged by {ctx.message.author}", color=2552230)                                      
-        await ctx.channel.send(embed=embedVar)
+async def purge(inter, limit: int):
+        await inter.channel.purge(limit=limit)
+    
+        embed = disnake.Embed(title="Purge Alert!", description=f"Messages Purged by {inter.author}", color=2552230) 
+      
+        await inter.channel.send(embed=embed)
 
 @client.event
-async def on_command_error(ctx, error):
-    await ctx.channel.purge(limit=1)
+async def on_command_error(inter, error):
+    await inter.channel.purge(limit=1)
     if isinstance(error, commands.CommandOnCooldown):
-        await ctx.send(f'This Command is in Cooldown! use it in {round(error.retry_after, 2)}')
+        await inter.send(f'This Command is in Cooldown! use it in {round(error.retry_after, 2)}')
 
 @clean.error
-async def clear_error(ctx, error):
+async def clear_error(inter, error):
     if isinstance(error, commands.MissingPermissions):
-      await ctx.send(f"{ctx.message.author.mention} You do not have Permissions to use this Command!")
+      await inter.send(f"{inter.author.mention} You do not have Permissions to use this Command!")
 
-@client.command(name="membercount", aliases=['mc'])
-async def membercount(ctx):
-  embedVar = discord.Embed(title=f"Members in {ctx.guild.name}", description=f"{ctx.guild.member_count}", color=2552230)
-  await ctx.reply(embed=embedVar)
+@client.slash_command(description="membercount of a server")
+async def membercount(inter):
+  embed = disnake.Embed(title=f"Members in {inter.guild.name}", description=f"{inter.guild.member_count}", color=2552230)
+  await inter.send(embed=embed)
+
+#@client.command(name="mute")
+#@commands.has_permissions(administrator=True)
+#async def mute(ctx, user : discord.Member,*, reason=None):
+  
+    #guild = ctx.guild
+    #role = ctx.guild.get_role(MUTE_ROLE)
     
-@client.command(name="mute")
-@commands.has_permissions(administrator=True)
-async def mute(ctx, user : discord.Member,*, reason=None):
+    #await user.add_roles(role)
+
+    #embedVar = discord.Embed(title="User Muted", description=f"{user.mention} has been muted by {ctx.author.mention} with the reason: {reason}")                                      
+    #await ctx.send(embed=embedVar)
+    #try:
+      #await user.send(f"you have been muted in {ctx.guild.name} with the Reason: {reason}")
   
-    guild = ctx.guild
-    role = ctx.guild.get_role(MUTE_ROLE)
+    #except:
+      #await ctx.send(f"unable to dm {user.mention}")
 
-    await user.add_roles(role)
+#@mute.error
+#async def clear_error(ctx, error):
+    #if isinstance(error, commands.MissingPermissions):
+        #await ctx.send(f"{ctx.message.author.mention} You do not have Permissions to use this Command!")
 
-    embedVar = discord.Embed(title="User Muted", description=f"{user.mention} has been muted by {ctx.author.mention} with the reason: {reason}")                                      
-    await ctx.send(embed=embedVar)
-    try:
-      await user.send(f"you have been muted in {ctx.guild.name} with the Reason: {reason}")
-  
-    except:
-      await ctx.send(f"unable to dm {user.mention}")
+#@client.command(name="unmute")
+#@commands.has_permissions(administrator=True)
+#async def unmute(ctx, user : discord.Member):
+  #guild = ctx.guild
+  #role = ctx.guild.get_role(MUTE_ROLE)
 
-@mute.error
-async def clear_error(ctx, error):
-    if isinstance(error, commands.MissingPermissions):
-        await ctx.send(f"{ctx.message.author.mention} You do not have Permissions to use this Command!")
+  #await user.remove_roles(role)
 
-@client.command(name="unmute")
-@commands.has_permissions(administrator=True)
-async def unmute(ctx, user : discord.Member):
-  guild = ctx.guild
-  role = ctx.guild.get_role(MUTE_ROLE)
+  #embedVar = discord.Embed(title="User Unmuted", description=f"{user.mention} has been unmuted by {ctx.author.mention}")                                      
+  #await ctx.send(embed=embedVar)
 
-  await user.remove_roles(role)
-
-  embedVar = discord.Embed(title="User Unmuted", description=f"{user.mention} has been unmuted by {ctx.author.mention}")                                      
-  await ctx.send(embed=embedVar)
-
-@unmute.error
-async def clear_error(ctx, error):
-    if isinstance(error, commands.MissingPermissions):
-        await ctx.send(f"{ctx.message.author.mention} You do not have Permissions to use this Command!")                                                                                 
+#@unmute.error
+#async def clear_error(ctx, error):
+    #if isinstance(error, commands.MissingPermissions):
+        #await ctx.send(f"{ctx.message.author.mention} You do not have Permissions to use this Command!")   
                   
-client.run('TOKEN') #use client.run(os.getenv('ENV_TOKEN') for replit dotenv
+# will test out 2 more commands in the next update     
+      
+from config import TOKEN
+client.run(TOKEN) #use client.run(os.getenv('ENV_TOKEN') for replit dotenv
